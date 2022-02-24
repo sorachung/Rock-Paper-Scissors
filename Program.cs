8624dbf07dc2882102fd98096ddb0098c18a7224
sorachung <sorachung.src@gmail.com>
2022-02-24 13:23:38 -0600

    now prints ascii art of rock, paper, and scissors

--- Program.cs
+++ Program.cs
@@ -1,12 +1,46 @@
 ﻿using System;
 
-namespace RockPaperScissors
+Main();
+
+void Main()
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+    PrintRock();
+    PrintPaper();
+    PrintScissors();
+}
+
+void PrintRock()
+{
+    Console.WriteLine(@"
+        _______
+    ---'   ____)
+          (_____)
+          (_____)
+          (____)
+    ---.__(___)
+    ");
+}
+
+void PrintPaper()
+{
+    Console.WriteLine(@"
+        _______
+    ---'    ____)____
+                ______)
+                _______)
+              _______)
+    ---.__________)
+    ");
+}
+
+void PrintScissors()
+{
+    Console.WriteLine(@"
+        _______
+    ---'   ____)____
+              ______)
+          __________)
+          (____)
+    ---.__(___)
+    ");
 }
